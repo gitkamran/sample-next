bad0a521254b6dfbba3f7b0073a43182cbbaf70d
gitkamran <mkamran.ir@gmail.com>
2023-09-22 15:20:26 +0330

    loader page rename

--- app/loader.js
+++ app/loader.js
@@ -1,5 +1,5 @@
 
-const loader = () => {
+const Loader = () => {
     return (
         <main className="flex items-center justify-center w-full min-h-screen">
             <h2 className="text-lg text-neutral-500">در حال بارگذاری اطلاعات...</h2>
@@ -7,4 +7,4 @@ const loader = () => {
     )
 }
 
-export default loader
+export default Loader
